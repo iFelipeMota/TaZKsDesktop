4e8c4b59ca2d08c8e5692ecda74b8b584b7e6ab1
Felipe <36648465+iFelipeMota@users.noreply.github.com>
2023-01-06 18:43:08 -0300

    Controler Pomodoro

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Main";
             this.Text = "TaZKs Desktop";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
 
         }
