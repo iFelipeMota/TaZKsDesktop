ff3cf47db8424d29f0e3a916b41843d2c0113560
Felipe <36648465+iFelipeMota@users.noreply.github.com>
2023-01-10 19:40:27 -0300

    Indicações de tasks na main

--- Controler/ProjectController.cs
+++ Controler/ProjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using TaZKsDesktop.Model;
 
 namespace TaZKsDesktop.Controler
@@ -11,13 +12,13 @@ namespace TaZKsDesktop.Controler
     {
         public Project GetProject(string projectName)
         {
-
+            return this.ReadProjectFile(projectName);
         } 
 
-        private void ReadProjectFile(string projectName)
+        private Project ReadProjectFile(string projectName)
         {
             FilesController file = new FilesController();
-
+            return file.GetProjectData(projectName);
         }
     }
 }
