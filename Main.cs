ce1f9f4675b55bce0791dd7c94855c8891d64cbf
Felipe <36648465+iFelipeMota@users.noreply.github.com>
2023-01-19 21:42:27 -0300

    Better Time text logic

--- Main.cs
+++ Main.cs
@@ -68,11 +68,7 @@ namespace TaZKsDesktop
         private void ChangeTextTime()
         {
             TimeSpan t = TimeSpan.FromSeconds(this.PomodoroTime);
-            this.TxtTime = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms",
-                t.Hours,
-                t.Minutes,
-                t.Seconds,
-                t.Milliseconds);
+            this.TxtTime = t.ToString(@"hh\:mm\:ss\:fff");
         }
     }
 }
